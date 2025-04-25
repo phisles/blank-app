800e5a838a1cf8bd05baafd863095d997cf6c59c
Philip Isles <156632514+phisles@users.noreply.github.com>
2025-04-25 06:28:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -277,3 +277,30 @@ if isinstance(activities_data, list) and activities_data:
     }), use_container_width=True)
 else:
     st.warning("No recent account activity found.")
+
+st.markdown("""
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright">
+    <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+      <span class="blue-text">Track all markets on TradingView</span>
+    </a>
+  </div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
+  {
+    "symbol": "NASDAQ:AAPL",
+    "width": "100%",
+    "height": "100%",
+    "locale": "en",
+    "dateRange": "1M",
+    "colorTheme": "dark",
+    "isTransparent": true,
+    "autosize": true,
+    "largeChartUrl": "",
+    "chartOnly": false
+  }
+  </script>
+</div>
+<!-- TradingView Widget END -->
+""", unsafe_allow_html=True)
